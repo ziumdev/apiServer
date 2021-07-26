025798b4bb568ff9eca5511ea2477d0f972ca7b1
CJM <cjm0904@ziumks.com>
2021-07-26 15:08:55 +0900

    first Commit

--- main.py
+++ main.py
@@ -12,15 +12,4 @@ def create_app(config_name):
 
 if __name__ == "__main__":
     env = sys.argv[1] if len(sys.argv) > 1 else 'dev'
-    create_app(env).run()
-    # if env == 'dev':
-    #     operationConfig = apiConfig.DevelopmentConfig
-    #     run(operationConfig)
-    #
-    # if env == 'test':
-    #     operationConfig = apiConfig.TestConfig
-    #     run(operationConfig)
-    #
-    # if env == 'prd':
-    #     operationConfig = apiConfig.ProductionConfig
-    #     run(operationConfig)
+    create_app(env).run(port=49999)
