f5ada05e422d40ac431aab385003894f070f2a0b
CJM <cjm0904@ziumks.com>
2021-07-26 15:14:28 +0900

    first Commit

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ def create_app(config_name):
 
 if __name__ == "__main__":
     env = sys.argv[1] if len(sys.argv) > 1 else 'dev'
-    create_app(env).run(port=49999)
+    create_app(env).run(host='221.155.136.103', port=49999)
